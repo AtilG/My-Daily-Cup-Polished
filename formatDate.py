dee7e8ed891b5d93a8c78ffec53c69f902db1c5f
Isaiah Isaac Bellanton <ibell8@Isaiahs-MacBook-Air.local>
2022-04-19 02:04:46 -0400

    Mad esome pylint corrections and fixed a bug in the Nasa Unit test

--- formatDate.py
+++ formatDate.py
@@ -1,5 +1,6 @@
-def formation(d):
+'''file to properly format our dates'''
+def formation(date):
     """Here will take the dateTime String, and format it in a way
     that we like"""
-    date = "%d/%d/%d" % (d.month, d.day, d.year)
-    return date
+    fulldate = "%d/%d/%d" % (date.month, date.day, date.year)
+    return fulldate
