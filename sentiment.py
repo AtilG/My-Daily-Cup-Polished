be58cdaa1ae762f37e1f6438e4a403c7ba1625b0
Isaiah Isaac Bellanton <ibell8@Isaiahs-MacBook-Air.local>
2022-04-19 01:57:04 -0400

    I made small changes to appease pylint rules

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,4 @@
+'''This file will handle our sentimental API'''
 import os
 import paralleldots
 from paralleldots import config
@@ -10,6 +11,7 @@ config.set_api_key(os.getenv("SENTIMENT_KEY"))
 
 # get the emotion of the text
 def get_emotion(entry):
+    '''This method will get the emotion for an individual journal entry'''
     tones = []
     emotions = paralleldots.emotion(entry)
 
@@ -17,25 +19,25 @@ def get_emotion(entry):
     emotions = emotions["emotion"].items()
     print(emotions)
 
-    max = {"emotion": "None", "data": -1}  # hold highest emotion
+    maximum = {"emotion": "None", "data": -1}  # hold highest emotion
     for data in emotions:
         # find maximum emotion value in case there is no value higher than 0.25
-        if data[1] > max["data"]:
-            max["emotion"] = data[0]
-            max["data"] = data[1]
+        if data[1] > maximum["data"]:
+            maximum["emotion"] = data[0]
+            maximum["data"] = data[1]
         # threshold for possible emotions conveyed
         if data[1] > 0.25:
             tones.append(data[0])
     # case to make sure a tone is shown
     if len(tones) == 0:
         try:
-            tones.append(max["emotion"])
+            tones.append(maximum["emotion"])
         except:
             tones.append("Bored")
 
     # correcting the 'fear' to 'fearful' for a better sound
-    for i in range(len(tones)):
-        if tones[i] == "Fear":
-            tones[i] = "Fearful"
+    for tone in tones:
+        if tone == 'Fear':
+            tone = 'Fearful'
 
     return tones
